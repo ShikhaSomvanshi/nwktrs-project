69848e69216b9ce4ce332df4d1d6273aeaf69a6a
Shikha Singh <shikha.singh694@gmail.com>
2020-04-06 17:52:17 +0530

    Display data from legacy db. No CSS present.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+#from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Traps(models.Model):
+    device = models.CharField(max_length=100, primary_key=True)
+    time = models.CharField(max_length=100)
+    data = JSONField()
+    # since device field need not be unique we need a id column to give us primary keys
+    # id = models.AutoField(primary_key=True)
+    #user = models.ForeignKey(User,on_delete=models.DO_NOTHING,)
+    #created = models.DateTimeField(auto_now_add=True)
+
+    #def __str__(self):
+    #    return self
+
+    class Meta:
+        managed = False
+        db_table = 'traps'
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Traps
 # Create your views here.
+def home(request):
+    #traps=Trap.objects
+    traps = Traps.objects.all()
+    print("Hi")
+    print(traps)
+    return render(request, 'traps/home.html', {'traps':traps})
